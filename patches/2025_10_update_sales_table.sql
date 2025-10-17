b95f2586a59e347144d7504f33f01b2e3927910c
Flaviano Angeli <42542455+fangeli0935@users.noreply.github.com>
2025-10-17 15:48:47 -0300

    Enhance sales table update with logging and rollback
    Added output messages for update execution and error handling.

--- patches/2025_10_update_sales_table.sql
+++ patches/2025_10_update_sales_table.sql
@@ -8,13 +8,18 @@ BEGIN
     DECLARE
        V_RC NUMBER;
     BEGIN
-       UPDATE MY_STAGING_TABLE SET CREATED_DATE = SYSDATE;
-       V_RC := SQL%ROWCOUNT;
-       COMMIT;
-       DBMS_OUTPUT.PUT_LINE('Affected line(s): '|| V_RC);
+        DBMS_OUTPUT.PUT_LINE('Applying update ...');
+        UPDATE MY_STAGING_TABLE SET CREATED_DATE = SYSDATE;
+        V_RC := SQL%ROWCOUNT;
+        COMMIT;
+        DBMS_OUTPUT.PUT_LINE('Affected line(s): '|| V_RC);
+        DBMS_OUTPUT.PUT_LINE('Execution done.');
     EXCEPTION 
         WHEN OTHERS 
-           THEN RAISE_APPLICATION_ERROR(-20001, SQLERRM);
+           THEN 
+              DBMS_OUTPUT.PUT_LINE('Something wrong! Applying rollback...');
+              ROLLBACK;
+              RAISE_APPLICATION_ERROR(-20001, SQLERRM);
     END;
 END;
 /
